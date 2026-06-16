8fc7d2a6410e85202917fa4163393c34e73a3e2e
Ericzhaoforu <Ericzhaoforu@users.noreply.github.com>
2026-06-16 07:53:45 +0000

    Deploying to gh-pages from @ Tongji-MRL/tongji-mrl.github.io@a03c63c918ffdce54f18822ff73b9ba16caf416a 🚀

--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "Ericzhaoforu/tongji-mrl.github.io",
+    "data-repo": "Tongji-MRL/tongji-mrl.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
